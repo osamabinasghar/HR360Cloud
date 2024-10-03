0ef4a85659991cfcf8b07fed1b0d0a55b1e43388
Osama Bin Asghar <Obinasghar@yahoo.com>
2024-10-03 13:12:25 +0500

    Tailwinds css installed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <h1 className="text-9xl text-black font-black leading-none mb-10 xl font-bold underline">
+        Hello world!
+    </h1>
+  )
+}
+
+export default Hero
